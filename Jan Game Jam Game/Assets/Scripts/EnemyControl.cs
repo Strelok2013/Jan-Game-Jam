179c61291dce8e503a1bf56e6c04bf86c84fc3a7
Paul King <PaulKing09@bigpond.com>
2023-01-17 21:47:44 +1100

    Updated EnemyControl, Created GameManager and updated Game Scene

--- Jan Game Jam Game/Assets/Scripts/EnemyControl.cs
+++ Jan Game Jam Game/Assets/Scripts/EnemyControl.cs
@@ -2,34 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AttackDir
+{
+    none,
+    up,
+    down,
+    left,
+    right,
+    upRight,
+    downLeft,
+    upLeft,
+    downRight    
+}
+
 public class EnemyControl : MonoBehaviour
 {
-
-    public enum Attacks
-    {
-        none,
-        up,
-        right,
-        down,
-        left,
-        upRight,
-        upLeft,
-        downRight,
-        downLeft
-    }
 
     // Have a member variable of player or the player has a member variable of enemy
     // to keep track of what attack is happening and so and so?
 
-    int[] m_attackPattern;
+    //int[] m_attackPattern;    
 
+    //public int m_numberOfAttacks = 8;
 
-    public int m_numberOfAttacks = 8;
+    public GameManager gameManager;
+
+    [Header("Attacks In Pattern")]
+    public int minAttacks = 3;
+    public int maxAttacks = 5;
+
+    [Range(0, 10)]
+    public float delayBetweenAttacks = 3;
+
+    Queue<AttackDir> m_attackPattern = new Queue<AttackDir>();
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        CreateAttackPattern();
     }
 
     // Update is called once per frame
@@ -38,6 +51,33 @@ public class EnemyControl : MonoBehaviour
         
     }
 
+    public void CreateAttackPattern()
+    {
+        int numberOfAttacks = Random.Range(minAttacks, maxAttacks);
+
+        for (int i = 0; i < numberOfAttacks; i++)// Generate enemy attack pattern???
+        {
+            int direction = Random.Range(1, 9);
+            m_attackPattern.Enqueue((AttackDir)direction);
+        }        
+
+        Invoke("PerformAttack", delayBetweenAttacks);
+    }
+
+    public void PerformAttack()
+    {
+        AttackDir direction = m_attackPattern.Dequeue();
+
+        gameManager.EnemyAttack = direction;
+        animator.SetInteger("direction", (int)direction);
+
+        if (m_attackPattern.Count > 0)
+            Invoke("PerformAttack", delayBetweenAttacks);
+        else
+            CreateAttackPattern();
+    }
+
+    /*
     public void EnemyAttackAlgorithm()
     {
         // Some random shit to pull attacks
@@ -45,51 +85,87 @@ public class EnemyControl : MonoBehaviour
 
         for (int i = 0; i < m_numberOfAttacks; i++)// Generate enemy attack pattern???
         {
-            m_attackPattern[i] = Random.Range(1, 9);
+
+            int direction = Random.Range(1, 9);
+            m_attackPattern.Enqueue((AttackDir)direction);
+
+            //m_attackPattern[i] = Random.Range(1, 9);
         }
 
         for (int i = 0; i < m_numberOfAttacks; i++)
         {
-            switch (m_attackPattern[i])
+            AttackDir direction = m_attackPattern.Dequeue();
+
+            animator.SetInteger("direction", (int)direction);
+            /*
+            switch (direction[i])
             {
-                case (int)Attacks.up:
+                case (int)AttackDir.up:
                     // Do animation 1 I guess...
                     EnemyAttack(m_attackPattern[i]);
                     break;
-                case (int)Attacks.down:
+                case (int)AttackDir.down:
                     EnemyAttack(m_attackPattern[i]);
                     break;
-                case (int)Attacks.left:
+                case (int)AttackDir.left:
                     EnemyAttack(m_attackPattern[i]);
                     break;
-                case (int)Attacks.right:
+                case (int)AttackDir.right:
                     EnemyAttack(m_attackPattern[i]);
                     break;
-                case (int)Attacks.upLeft:
+                case (int)AttackDir.upLeft:
                     EnemyAttack(m_attackPattern[i]);
                     break;
-                case (int)Attacks.upRight:
+                case (int)AttackDir.upRight:
                     EnemyAttack(m_attackPattern[i]);
                     break;
-                case (int)Attacks.downLeft:
+                case (int)AttackDir.downLeft:
                     EnemyAttack(m_attackPattern[i]);
                     break;
-                case (int)Attacks.downRight:
+                case (int)AttackDir.downRight:
                     EnemyAttack(m_attackPattern[i]);
                     break;
             }
         }
-    }
+    }*/
 
-    void EnemyAttack(int direction)
+    /*
+    void EnemyAttack(AttackDir direction)
     {
         switch (direction)
         {
-            case 1:
+            case AttackDir.up:
                 // Do animtion one??
                 
                 break;
+
+            case AttackDir.down:
+
+                break;
+
+            case AttackDir.left:
+
+                break;
+
+            case AttackDir.right:
+
+                break;
+
+            case AttackDir.upLeft:
+
+                break;
+
+            case AttackDir.downLeft:
+                break;
+
+            case AttackDir.upRight:
+
+                break;
+
+            case AttackDir.downRight:
+
+                break;
         }
-    }
+    }*/
 
 }
---
+++ Jan Game Jam Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // properties
+    public AttackDir PlayerAttack { get; set; } = AttackDir.none;
+    public AttackDir EnemyAttack { get; set; } = AttackDir.none;
+
+    public int Score { get; private set; } = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CompareAttacks()
+    {
+        if(PlayerAttack == AttackDir.none)
+        {
+            // damage player          
+        }
+        else
+        {
+            int enemyDirection = (int)EnemyAttack;
+            int playerDirection = (int)PlayerAttack;
+
+            if(enemyDirection%2 == 1) // odd direction
+            {
+                if(playerDirection == enemyDirection + 1) // opposite direction
+                {
+                    // successful parry
+                }
+                else
+                {
+                    // damage player
+                }
+            }
+            else // even direction
+            {
+                if (playerDirection == enemyDirection - 1) // opposite direction
+                {
+                    // successful parry
+                }
+                else
+                {
+                    // damage player
+                }
+            }
+
+        }
+
+    }
+
+}
